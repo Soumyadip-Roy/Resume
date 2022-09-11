31cf1b98536e0197fd286a59016e2468ecbe0f68
Soumyadip Roy <85121861+Soumyadip-Roy@users.noreply.github.com>
2022-09-12 01:14:24 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,11 @@
+// let root = document.querySelector(':root')
+// let rootStyles = getComputedStyle(root);
+// const primaryLight = rootStyles.getPropertyValue('--primaryLight')
+// const primaryDark = rootStyles.getPropertyValue('--primaryDark')
+// body = document.querySelector('body')
+
+// body.addEventListener('click',()=>{
+
+//     body.style.background = primaryLight;
+//     body.style.color = primaryDark;
+// })
